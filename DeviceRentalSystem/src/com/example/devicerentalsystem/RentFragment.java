b4c8281a97a4a30cfa80ee147bac8d73bf3215e2
abuzakaria <zakariatanim@gmail.com>
2014-09-14 16:33:55 +0200

    app skeleton update. added navigation drawer and separate blank activities. custom icons are added too. colors fixed.

---
+++ DeviceRentalSystem/src/com/example/devicerentalsystem/RentFragment.java
@@ -0,0 +1,20 @@
+package com.example.devicerentalsystem;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RentFragment extends Fragment {
+	
+	public RentFragment(){}	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		 
+        View rootView = inflater.inflate(R.layout.fragment_rent, container, false);
+         
+        return rootView;
+    }
+
+}
---
+++ DeviceRentalSystem/src/com/example/devicerentalsystem/ReturnFragment.java
@@ -0,0 +1,20 @@
+package com.example.devicerentalsystem;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReturnFragment extends Fragment {
+	
+	public ReturnFragment(){}	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_return, container, false);
+         
+        return rootView;
+    }
+
+}
