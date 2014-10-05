3065b92923e875312531d11c13cc7aca4696d190
abuzakaria <zakariatanim@gmail.com>
2014-10-05 18:00:01 +0200

    signature pad implemented. minor ui tweak

---
+++ DeviceRentalSystem/libs/lib-signaturepad/gen/com/github/gcacace/signaturepad/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package gen.com.github.gcacace.signaturepad;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
